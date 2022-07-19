9f27b370b186d2fa6a0e5b9b5df458ff42b39694
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-07-19 13:32:34 +0300

    responsive + statistics

--- components/PostTile.tsx
+++ components/PostTile.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { formatDate } from '../utils/format';
 import CategoriesBadge from './CategoriesBadge';
 import Keywords from './KeywordsBadge';
+import { HeartIcon, ChatIcon } from '@heroicons/react/outline';
 
 type PostTilePropsType = {
   post: IPost;
@@ -14,8 +15,8 @@ const PostTile = ({ post }: PostTilePropsType) => {
     post.fields;
 
   return (
-    <div className="flex p-2 my-2 bg-white border border-1 shadow-sm lg:flex-col">
-      <div className="flex w-1/3 lg:w-full">
+    <div className="flex p-2 my-2 bg-white border border-1 shadow-sm flex-col">
+      <div className="flex w-full">
         <Image
           src={`https:${poster?.fields.file.url || ''}`}
           alt={poster?.fields.title}
@@ -27,26 +28,40 @@ const PostTile = ({ post }: PostTilePropsType) => {
           objectPosition="center"
         />
       </div>
-      <div className="flex flex-col w-2/3 lg:w-full lg:mt-4 ml-2">
-        <div className="flex lg:justify-start sm:justify-between">
+      <div className="flex flex-col mt-2">
+        <div className="flex my-1">
           <h5 className="flex text-sm text-gray-600">
             {formatDate(post.sys.createdAt)}
           </h5>
-          <div className="lg:ml-4">
+          <div className="ml-4">
             <CategoriesBadge categories={categories} />
           </div>
         </div>
-        <h2 className="flex font-bold text-2xl text-gray-700">{title}</h2>
-        <div className="flex flex-col mb-2">
+        <Link href={`/posts/${slug}`}>
+          <h2 className="flex font-bold text-2xl text-gray-700 my-1 cursor-pointer hover:text-gray-500">
+            {title}
+          </h2>
+        </Link>
+        <div className="flex flex-col my-1">
           <Keywords keywords={keywords} />
         </div>
-        <div className="flex mb-auto text-gray-700">{shortDescription}</div>
-        <div className="flex sm:justify-end lg:justify-start mt-4">
+        <div className="flex text-gray-700 my-1">{shortDescription}</div>
+        <div className="flex justify-start my-1">
           <Link href={`/posts/${slug}`}>
             <a className="flex w-36 h-10 border hover:bg-gray-50 justify-center items-center">
               Read more
             </a>
           </Link>
+        </div>
+        <div className="flex flex-row text-thin text-sm text-gray-600 my-1">
+          <div className="flex items-center flex-row ml-2">
+            <HeartIcon className="h-5 w-5" />
+            <span className="ml-2">5</span>
+          </div>
+          <div className="flex items-center flex-row ml-2">
+            <ChatIcon className="h-5 w-5" />
+            <span className="ml-2">19</span>
+          </div>
         </div>
       </div>
     </div>
