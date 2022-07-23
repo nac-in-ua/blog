802bc513c74090255aba4e3edc11672b42b3f9aa
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-07-23 19:21:33 +0300

    added graphql query

--- contentful/getData.ts
+++ contentful/getData.ts
@@ -7,6 +7,11 @@ const client = createClient({
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
 });
 
+const getAccess = () => ({
+  space: process.env.CONTENTFUL_SPACE_ID as string,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+});
+
 const getPostData = (post: Entry<IPostFields>): PostData => {
   const { fields } = post;
   return {
@@ -36,6 +41,58 @@ const getData = async (): Promise<DataType> => {
 
   const posts = postEntries.items.map(getPostData);
   const categories = categoryEntries.items.map(getCategoryData);
+
+  const getDataByQuery = async (query: string) => {
+    const response = await fetch(
+      `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+        },
+        body: JSON.stringify({
+          query,
+        }),
+      }
+    );
+    return response.json();
+  };
+  const {
+    data: { postCollection },
+  } = await getDataByQuery(`
+  {
+    postCollection {
+      items {
+        title
+        body {
+          json
+        }
+        poster {
+          title
+          description
+          contentType
+          fileName
+          size
+          url
+          width
+          height
+        }
+        slug
+        shortDescription
+        categoriesCollection {
+          items {
+            name
+          }
+        }
+        keywords
+        sys {
+          publishedAt
+        }
+      }
+    }
+  }
+`);
 
   return {
     posts,
