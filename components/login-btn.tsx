d1582314725ad3a4008a4f0974857c1a48764a3b
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-09-20 01:05:52 +0300

    re-designaed login button

--- components/login-btn.tsx
+++ components/login-btn.tsx
@@ -1,10 +1,20 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
 export default function Component() {
   const { data: session } = useSession();
   if (session) {
     return (
       <>
-        Signed in as {session.user.email} <br />
+        <div className="flex flex-row items-center gap-2">
+          <Image
+            className="rounded-full"
+            src={session.user.image as string}
+            alt="user image"
+            width={40}
+            height={40}
+          />
+          <span>{session.user.name}</span>
+        </div>
         <button onClick={() => signOut()}>Sign out</button>
       </>
     );
