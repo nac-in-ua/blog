3ebb132c64d69f00c75d9a6435bc61855e9011b1
Oleksandr <alexandr.nedilko@gmail.com>
2022-09-22 22:23:57 +0300

    fixed typings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import GeneralLayout from '../components/Layouts/General';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, SessionProviderProps } from 'next-auth/react';
+import { CategoriesItem, Panel } from '../hygraph/Panel';
 
-function MyApp({ Component, pageProps }: AppProps) {
+type BlogAppProps = AppProps & {
+  pageProps: AppProps['pageProps'] & {
+    categories: CategoriesItem[];
+    panel: Panel;
+    session: SessionProviderProps['session'];
+  };
+};
+
+function MyApp({ Component, pageProps }: BlogAppProps) {
   const { categories, panel } = pageProps;
 
   return (
